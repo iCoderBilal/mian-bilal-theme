a907c4236eab0e8924549bef608ca742938e062e
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-07-24 01:51:15 +0500

    popup

---
+++ assets/product-grid.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productBlocks = document.querySelectorAll(".product-block");
+
+    const popup = document.createElement("div");
+
+    popup.id = "product-popup";
+    popup.style.display = "none";
+    document.body.appendChild(popup);
+
+    productBlocks.forEach(block => {
+        const dot = block.querySelector(".product-dot");
+        dot.addEventListener("click", function (event) {
+            event.stopPropagation();
+            const productHandle = block.dataset.productHandle;
+            fetch(/product/${ productHandle }.js)
+                .then(response => response.json())
+                .then(product => {
+                    let variantHtml = "";
+                    product.variants.forEach(variant => {
+                        variantHtml += <option value="${variant.id}">${ variant.title } - ${ variant.price / 100 } ${ shopify.currency.active }</option>;
+                    });
+
+                    popup.innerHTML = `
+                <div class="popup-content">
+                <h1> This is the Pop Up</h1>
+                </div>
+                `
+                })
+        })
+    })
+
+})
